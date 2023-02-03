4a12f30ffd923a315f38eca1513fa79f4b1c9252
Pipperonni <blueas2019@gmail.com>
2023-02-03 12:03:05 -0600

    test

---
+++ parking_g.py
@@ -0,0 +1,30 @@
+
+# class ParkingGargage:
+
+#     def __init__(self, parking_space, ticket_number)
+#         self.parking_space = parking_space
+#         self.ticket_number = ticket_number
+
+
+# class TakeTicket:
+
+
+
+# class PayForTicket:
+
+
+# class LeavingGargage:
+
+
+
+tickets = 1
+print(tickets)
+
+
+# current_ticket = {
+#     ticket_number{
+#         paid:# False or True if prepaid
+#         time_parked:# charged for ever 15min for parking       
+#     }
+#     parking_space:# True if in use
+# }
