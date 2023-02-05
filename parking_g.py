3db74b98099fb727da11b5b3d3ab47f9a8a02394
Pipperonni <blueas2019@gmail.com>
2023-02-04 21:35:51 -0600

    update

--- parking_g.py
+++ parking_g.py
@@ -1,59 +1,63 @@
 
 tickets = [num for num in range(1,3)]
 spaces = [num for num in range(1,3)]
-
+current_ticket = {}
 
 
 class ParkingGargage():
 
     def __init__(self):
-        self.current_ticket = []
+        self.ticket_test = {}
         
     
-    def space_change(self):
-        spaces.pop()
-        self.ticket_tracking()
+    def space_parking(self):
+        if len(spaces) == 0:
+            print("Sorry, there are no available parking spots. Please come back later.")
+        else:
+            spaces.pop()
+            self.ticket_tracking()
     
     def ticket_tracking(self):
         user_ticket = tickets.pop()
-        for i in range(len(tickets)):
-            if i == user_ticket:
-                print(f"Ticket #{user_ticket}")
+        print(f"Your ticket #{str(user_ticket)} in space #{user_ticket}")
         users_choice = input("Would you like to pay now? (y/n): ")
         if users_choice == "y":
+            current_ticket[f"{user_ticket}"] = True
+            print(current_ticket)
             prep = Paid(pre_paid="", pay_later="")
             prep.pre_pay()
-            
-
-
-    def ticket_pay(self): 
-        pass
+        elif users_choice == "n":
+            current_ticket[f"{user_ticket}"] = False
+            return          
     
+    def leaving(self):
+        while True:    
+            user_show_ticket = input("What is your ticket number?: ")
+            if current_ticket[user_show_ticket] == True:    
+                print("Have a nice day.")
+                tickets.append(user_show_ticket)
+                spaces.append(user_show_ticket)
+                return
+            elif current_ticket[user_show_ticket] == False:
+                prep2 = Paid(pre_paid="", pay_later="")
+                prep2.later_pay()
+            else:
+                print("Invald Input")
+                continue
+
     def run(self):
-        user_choice = input("Are you parking: (y/n): ") 
-        if user_choice == "y":
-            self.space_change()
-        elif user_choice == "n":
-            return
+        while True:
+            user_choice = input("Are you parking: (y/n) or quit: ") 
+            if user_choice == "y":
+                self.space_parking()
+            elif user_choice == "n":
+                self.leaving()
+            elif user_choice == "quit":
+                break
+            else:
+                print("Invald Input")
+                continue
             
-    
-    def leaving():
-         pass
-
-# class Ticket(ParkingGargage):
-
-#     def __init__(self, spaces, tickets, show_if_paid):
-#         super().__init__(self, spaces, tickets)
-#         self.show_if_paid = show_if_paid
-
-#     def take_ticket(self): # take ticket
-#         pass # printed message "Welcome, please take your ticket."
-    # input("To pay now enter 1, for later enter 2:  ")
-                    # If user enters 1, (go to paid function) also, decrease both tickets list and spaces list by 1
-                    # If user enters 2, update current_ticket dictionary to False(pay later), decrease both tickets list, and spaces list by 1, also quit program
-    
-    #def print_ticket_tf()           
-        #pass
     
 class Paid():
 
@@ -62,20 +66,30 @@ class Paid():
         self.pay_later = pay_later
 
     def pre_pay(self):
-        print("Please pay $5.00: (y/n): ")
-        # paid function for parking before and after parking
-            
-            # Pre-pay function: if user inputs "now": input("Please pay $5.00: (y/n): ")
-                # If "y" then update current_ticket dictionary to True(they pre-paid) end program
-                # If "n"  input("To pay now enter 1, for later enter 2:  ")
-                    # If user enters 2, update current_ticket dictionary to False(pay later), end program
+        while True:
+            user_choice = input("Please pay $5.00: (y/n): ")
+            if user_choice == "y":
+                print("Ticket has been paid, you have 15min to leave.")
+                back_to_run = ParkingGargage()
+                back_to_run.run()
+            elif user_choice == "n":
+                continue
+            else:
+                print("Invald Input")
+                continue
 
-            # Pay when leaving function:
-                # input("Please pay $5.00: (y/n) ")
-                    # If user enters "y" print "Have a nice day."
-                    # If user enters "n" repeat prompt: input("Please pay $5.00: (y/n) ")
-
-# For All Inputs: if they do not enter the correct inputs prompt message: "Input Invalid
+    def later_pay(self):
+        while True:
+            user_choice = input("Please pay $5.00: (y/n): ")
+            if user_choice == "y":
+                print("Have a nice day.")
+                back_to_run = ParkingGargage()
+                back_to_run.run()
+            elif user_choice == "n":
+                continue
+            else:
+                print("Invald Input")
+                continue
 
 new_customer = ParkingGargage()
 new_customer.run() 
